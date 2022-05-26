7ad06a164d32d759bb555fcbea87d9a9da106528
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-26 13:53:42 +0530

    [Tushar Vidwans] Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Logical_Programs_Day6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Logical Programs, in this we are going to write multiple Logical Programs");
+
+        }
+    }
+}
